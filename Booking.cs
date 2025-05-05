58a5f20af40430c10d90ba8114b8a216aa47bd35
Ali Bagheri <23119az@stmarys.ac.uk>
2025-05-05 20:16:33 +0100

    init

---
+++ Booking.cs
@@ -0,0 +1,13 @@
+﻿namespace cloud_backend
+{
+    public record Booking(
+    int Id,
+    string Title,
+    string FirstName,
+    string Surname,
+    string Email,
+    int RoomId,
+    string CheckInDate,
+    string CheckOutDate
+);
+}
